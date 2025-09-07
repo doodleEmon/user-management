342589dade0da2d904ef75e47c781375eaa08986
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-07 13:14:13 +0600

    search functionality done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import SearchBar from "@/components/searchBar";
+import UserDirectory from "@/components/userDirectory";
 
 export default function Home() {
   return (
-    <div className="mx-12 my-8 shadow rounded bg-white p-6">
-      <h3 className="text-2xl font-semibold">User Management</h3>
-      <SearchBar />
+    <div className="mx-12 my-8 shadow-md rounded-lg bg-white p-6">
+      <a href="/" className="text-2xl font-semibold">User Management</a>
+      <UserDirectory />
     </div>
   );
 }
---
+++ src/components/userDirectory.tsx
@@ -0,0 +1,103 @@
+'use client'
+
+import React, { useEffect, useMemo, useState } from 'react'
+import { User } from '@/types/user'
+
+export default function UserDirectory() {
+    const [inputValue, setInputValue] = useState("");
+    const [searchQuery, setSearchQuery] = useState("");
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetch('https://jsonplaceholder.typicode.com/users');
+            const users = await data.json();
+            setUsers(users);
+        };
+        fetchData();
+    }, []);
+
+    const filteredUsers = useMemo(() => {
+        if (!searchQuery) return users;
+
+        return users.filter((user: User) =>
+            user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.username.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+    }, [users, searchQuery]);
+
+    // Handle Enter press
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            setSearchQuery(inputValue.trim());
+        }
+    };
+
+    // Handle Search button
+    const handleSearch = () => {
+        setSearchQuery(inputValue.trim());
+    };
+
+    return (
+        <div className='mt-7'>
+            {/* search bar */}
+            <div className='flex items-center gap-4'>
+                <div className='flex-1 relative'>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        onKeyDown={handleKeyDown}
+                        className='border border-gray-300 focus:outline-blue-600 py-[10px] px-3 w-full rounded-lg'
+                        placeholder='Search by name or email'
+                    />
+                    {
+                        inputValue !== "" &&
+                        <button onClick={() => (
+                            setSearchQuery(""),
+                            setInputValue("")
+                        )} className='absolute right-4 top-1/2 -translate-y-1/2 bg-gray-500 text-white size-6 rounded-full cursor-pointer z-50'>X</button>
+                    }
+                </div>
+                <button
+                    type='button'
+                    onClick={handleSearch}
+                    className='p-2 bg-blue-600 hover:bg-blue-700 text-white text-lg px-6 rounded-lg cursor-pointer'
+                >
+                    Search
+                </button>
+            </div>
+
+            {/* user table */}
+            {
+                filteredUsers.length === 0 ? (<div className='w-full mt-8 text-center text-lg font-medium'>No data found!</div>) : (<table className='w-full mt-8'>
+                    <thead>
+                        <tr className='bg-gray-100'>
+                            <th className='text-start px-5 py-2 font-normal text-gray-600 uppercase text-sm'>Name</th>
+                            <th className='text-start px-5 py-2 font-normal text-gray-600 uppercase text-sm'>Email</th>
+                            <th className='text-start px-5 py-2 font-normal text-gray-600 uppercase text-sm'>Phone</th>
+                            <th className='text-start px-5 py-2 font-normal text-gray-600 uppercase text-sm'>Company</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredUsers.map((user: User, index: number) => (
+                            <tr
+                                key={index}
+                                className={`hover:bg-gray-100 cursor-pointer ${index !== filteredUsers.length - 1 ? 'border-b border-b-gray-200' : ''}`}
+                            >
+                                <td className='text-start px-5 py-3.5 font-normal text-[14.5px] text-gray-800'>
+                                    <p>{user.name}</p>
+                                    <p className='text-gray-600'>@{user.username}</p>
+                                </td>
+                                <td className='text-start px-5 py-3.5 font-normal text-[14.5px] text-gray-800'>{user.email}</td>
+                                <td className='text-start px-5 py-3.5 font-normal text-[14.5px] text-gray-800'>{user.phone}</td>
+                                <td className='text-start px-5 py-3.5 font-normal text-[14.5px] text-gray-800'>{user.company.name}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>)
+            }
+        </div>
+    )
+}
