4cfb41947583f29228f4fd2bb4c205fd95b70d02
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-09-07 10:42:03 +0600

    searchbar component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import SearchBar from "@/components/searchBar";
+
 export default function Home() {
   return (
-    <div>
-      hello nextjs
+    <div className="mx-12 my-8 shadow rounded bg-white p-6">
+      <h3 className="text-2xl font-semibold">User Management</h3>
+      <SearchBar />
     </div>
   );
 }
